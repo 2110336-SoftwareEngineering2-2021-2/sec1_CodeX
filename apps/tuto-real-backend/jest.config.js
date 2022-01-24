d7166b2608a0915583b75c814555078c994ad80a
Veerin Phana-ngam <beam7684@gmail.com>
2022-01-24 18:20:29 +0700

    initial commit

---
+++ apps/tuto-real-backend/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'tuto-real-backend',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/tuto-real-backend',
+};
