6f2ffdf6194a6de4d44800f71b2e911c1088ff98
Ruttasate <m18260.29@gmail.com>
2022-01-31 20:21:33 +0900

    [Front] Add new global color

--- apps/tuto-real-frontend/src/app/constants/color.js
+++ apps/tuto-real-frontend/src/app/constants/color.js
@@ -4,10 +4,13 @@ const COLORS = {
   third: "#52AB98", 
 
   gray: "#F2F2F2",
+  borderGray: "#B0B4BA",
   darkgray: "#CCD0D5",
 
   white: "#FFFFFF",
   black: "#000000"
+
+
 }
 
 export default COLORS
