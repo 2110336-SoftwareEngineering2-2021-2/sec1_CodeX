917f8403e27883926c0278d76829b38a30d0aab3
Ruttasate <m18260.29@gmail.com>
2022-02-03 20:05:53 +0900

    [Front] Link with modal and add image

--- apps/tuto-real-frontend/src/app/pages/ChangeAccountApprovePage.js
+++ apps/tuto-real-frontend/src/app/pages/ChangeAccountApprovePage.js
@@ -1,7 +1,8 @@
 import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
+import ModalTwoButton from '../components/modal/ModalTwoButton';
 
-const ChangeAccountTypePage = (props) => {
+const ChangeAccountApprovePage = (props) => {
 
     const back = '< Back'
     const [showModalApprove,setShowModalApprove] = useState(false)
@@ -31,13 +32,13 @@ const ChangeAccountTypePage = (props) => {
           <p className='header' style={{width: '100%'}}>Please check the user infomation below.</p>
           <hr/>
           
-          <div className='section'>
+          <div className='section' style={{alignItems: 'flex-start'}}>
             <p className='header' style={{width: '30%'}}>COPY OF CITIZEN ID CARD</p>
             <img src={imageCid} className='upload-image' style={{width: '62%', margin: '1%'}} alt='imageCid'/>
           </div>
           <hr/>
           
-          <div className='section'>
+          <div className='section' style={{alignItems: 'flex-start'}}>
             <p className='header' style={{width: '30%'}}>TRANSCRIPTION</p>
             <img src={imageTrans} className='upload-image' style={{width: '62%', margin: '1%'}} alt='imageTrans' />
           </div>
@@ -47,8 +48,32 @@ const ChangeAccountTypePage = (props) => {
             <button className='submit-open shadow' onClick={handleReject} style={{marginLeft: '2%', backgroundColor: 'red'}}>Reject</button>
             <button className='submit-open shadow' onClick={handleApprove} style={{marginLeft: '2%'}}>Approve</button>
         </div>
+
+        <ModalTwoButton 
+            show={showModalApprove} 
+            setShow={setShowModalApprove} 
+            title='Please confirm the approvol' 
+            header='The user will become a tutor role. Are you sure?' 
+            leftTo='/' 
+            leftMessage='Approve'
+            rightMessage='cancel'
+            leftColor='var(--third)'
+            rightColor='var(--yellow)'
+        />
+
+        <ModalTwoButton 
+            show={showModalReject} 
+            setShow={setShowModalReject} 
+            title='Please confirm the reject' 
+            header='The request will be deleted. Are you sure?' 
+            leftTo='/' 
+            leftMessage='Reject'
+            rightMessage='cancel'
+            leftColor='red'
+            rightColor='var(--yellow)'
+        />
     </div>
   );
 };
 
-export default ChangeAccountTypePage;
+export default ChangeAccountApprovePage;
