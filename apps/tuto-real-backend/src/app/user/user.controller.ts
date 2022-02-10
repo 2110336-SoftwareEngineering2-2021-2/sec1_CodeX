c18be53f340f850a891cb79a6dfb678873cdcd82
shane53386 <mchutipon@gmail.com>
2022-02-10 15:00:49 +0700

    [Front] New signUp

--- apps/tuto-real-backend/src/app/user/user.controller.ts
+++ apps/tuto-real-backend/src/app/user/user.controller.ts
@@ -3,16 +3,11 @@ import {
   Controller,
   Get,
   Param,
-  ParseIntPipe,
   Patch,
   Post,
-  UploadedFile,
-  UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
 import { updateUserDto } from './updateUser.dto';
 import { UserDto } from './user.dto';
-import { User } from './user.interface';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -20,23 +15,20 @@ export class UserController {
   constructor(private readonly service: UserService) {}
 
   @Get(':email')
-  GetProfileByMail(@Param('email') mail: String) {
-    return this.service.GetProfileByMail(mail);
+  getProfileByMail(@Param('email') email: String) {
+    return this.service.getProfileByMail(email);
   }
 
   @Post('/create')
-  Create(@Body() dto: UserDto) {
-    dto.role = 'Student';
+  createProfile(@Body() dto: UserDto) {
     dto.profileImg = {
       url: 'https://storage.googleapis.com/codex_img/Profile/default.jpg',
     };
-    return this.service.Create(dto);
+    return this.service.createProfile(dto);
   }
 
   @Patch(':email')
-  update(@Param('email') email: string, @Body() dto: updateUserDto) {
+  updateProfile(@Param('email') email: string, @Body() dto: updateUserDto) {
     return this.service.updateProfile(email, dto);
   }
-
- 
 }
--- apps/tuto-real-backend/src/app/user/user.module.ts
+++ apps/tuto-real-backend/src/app/user/user.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
-import { UserSchema } from './user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { UserSchema } from './user.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
