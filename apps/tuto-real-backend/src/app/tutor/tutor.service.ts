92367d8bbd62c5d5734be67817b6f60e1b12b11d
sorasit wangsathitwong <sorasit789@gmail.com>
2022-02-17 14:12:33 +0700

    [Back] search

--- apps/tuto-real-backend/src/app/tutor/tutor.service.ts
+++ apps/tuto-real-backend/src/app/tutor/tutor.service.ts
@@ -5,10 +5,12 @@ import { UserDto } from '../user/user.dto';
 import { User } from '../user/user.interface';
 import { sendMail } from '../util/google';
 import { CriteriaDto } from './cirteria.dto';
+import { Schedule } from './schedule.interface';
 
 @Injectable()
 export class TutorService {
-  constructor(@InjectModel('User') private tutorModel: Model<User>) {}
+  constructor(@InjectModel('User') private tutorModel: Model<User> ,
+  @InjectModel('Schedule') private scheduleModel: Model<Schedule>  ) {}
 
   public async getTutor(): Promise<UserDto[]> {
     const tutors = this.tutorModel.find({ role: 'Tutor' }).exec();
@@ -16,39 +18,72 @@ export class TutorService {
   }
 
   public async searchTutor(dto : CriteriaDto) : Promise<any> {
-  var queryKeyword = [] ,queryDays = []
-  console.log(dto)
+  var queryKeyword = [],queryDays = []
+  //console.log(dto)
   if (!!dto.keyword){
     dto.keyword.forEach((word)=>{
         if (word.length>=3)
-        queryKeyword.push({$or : [{description : {$regex:word}},
-                                {firstName : {$regex:word}} ,
-                                {lastName : {$regex:word}}]})
-    })
-  }
+          queryKeyword.push({$or : [{description : {$regex:word, $options : 'i'}},
+                                {firstName : {$regex:word, $options : 'i'}} ,
+                                {lastName : {$regex:word, $options : 'i'}},
+                                {"schedules.days.slots.description" : {$regex:word, $options : 'i'}}
+                              ]})
+    })}
   if (!!dto.days){
     var tmp
     dto.days.forEach((day)=>{
-      tmp = `schedule.${day}`
-        queryDays.push({ [tmp] :{$exists:true}})
+      tmp = "schedules.days.day"
+        queryDays.push({ [tmp] : day})
     })
   }
-  console.log(queryDays)
-  var result = await this.tutorModel.find(
-    {
-      $and :[
-        (!!dto.keyword && queryKeyword.length!=0)? { $and :  queryKeyword} :{} ,
-        //{ ratePrice : (!!dto.ratePrice)? { $gt :  dto.ratePrice.min, $lt : dto.ratePrice.max}:{}} ,
-        (!!dto.subjects)? { subjects:  { $all: dto.subjects }} : {},
-        {role : "Tutor"},
-        (!!dto.days)? {$and : queryDays} : {} 
-        
-      ]
-    }
-    )
-  console.log(result)
-    return result
 
+   var join = await this.tutorModel
+   .aggregate([
+    {$match : {$and :[
+      (!!dto.subjects)? { subjects:  { $all: dto.subjects }} : {},
+      {sid:{$exists : true}},
+      {role : "Tutor"}      
+  ]}},
+  {
+    $project: {
+      "sid": {
+          $map: {
+          input: "$sid",
+          in: { $toObjectId: "$$this" }
+        }
+      }
+    }
+  }
+    ,
+    {$lookup: {
+              from: "schedules",
+              localField: "sid",
+              foreignField: "_id",
+              as: "schedules"
+          }
+  },{
+    $match: {$and :[
+        (!!dto.days)? {$and : queryDays} : {} ,
+        (!!dto.rate)? { "schedules.pricePerSlot" : { $gte :  dto.rate.min, $lte : dto.rate.max}}:{} ,
+        (!!dto.keyword && queryKeyword.length!=0)? { $and :  queryKeyword} :{}
+    ]}
+  }])
+  console.log(join)
+  return join
+  /*return await this.tutorModel.find({ $where: function () {
+    var search_key = "description";
+
+    function check_key(document) {
+      return Object.keys(document).some(function(key) {
+        if ( typeof(document[key]) == "object" ) {
+            return check_key(document[key]);
+        } else if (key==search_key){
+          return document[key].includes("Enjoy");
+        }
+      });
+    }
+    return check_key(this);
+  }})*/
   }
 
   async send(){
