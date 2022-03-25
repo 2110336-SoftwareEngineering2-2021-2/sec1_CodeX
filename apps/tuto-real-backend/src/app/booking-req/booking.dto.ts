cc8ebe8c3c267c8eb22cfd7203f6996c0567cb5a
Suppawich Tawornpichayachai <upsaga77@hotmail.com>
2022-03-25 20:14:08 +0700

    [Back] 70% of updateBooking

--- apps/tuto-real-backend/src/app/booking-req/booking.dto.ts
+++ apps/tuto-real-backend/src/app/booking-req/booking.dto.ts
@@ -2,7 +2,7 @@ import { TimePeriodDto } from '../util/timePeriod.dto';
 
 export class BookingDto {
   student_id: String;
-  schedule_id: String;
+  readonly schedule_id: String;
   readonly days: [{ _id: false; day: String; slots: [Number] }];
   timeStamp: Date;
   totalPrice: Number;
