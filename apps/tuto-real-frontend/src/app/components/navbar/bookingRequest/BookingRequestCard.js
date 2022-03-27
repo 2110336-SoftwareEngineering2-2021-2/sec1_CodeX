5dd62fe55737e49839b5a0b3413f95ed83942076
kharit <6230054221@student.chula.ac.th>
2022-03-27 13:24:24 +0700

    [Front] link GET to bookingRequest overlay

--- apps/tuto-real-frontend/src/app/components/navbar/bookingRequest/BookingRequestCard.js
+++ apps/tuto-real-frontend/src/app/components/navbar/bookingRequest/BookingRequestCard.js
@@ -1,42 +1,122 @@
 import COLORS from "../../../constants/color"
 import { useState } from 'react';
-import ModalTwoButton from '../../modal/ModalTwoButton';
 
 const BookingRequestCard= (prop) => {
-    const {status, requestTime, tutorName, totalPrice, subjectList, setShowModal,setShow} = prop
+    const {
+        bookingId,
+        requestTime, 
+        studentName, 
+        totalPrice, 
+        days,
+        setShowModal,
+        setShow
+        } = prop;
 
-    const genBorderColor = (status) => {
-        if (status === "Canceled") return "#FBE2C5"
-        else if (status === "Accepted") return "#D3EAE5"
-        else if (status === "Rejected") return "#FFD6C9"
-        else return COLORS.lightgray
-    }
-    const genStatusColor = (status) => {
-        if (status === "Canceled") return "#EF8C18"
-        else if (status === "Accepted") return COLORS.third
-        else if (status === "Rejected") return "#FF5D29"
-        else return COLORS.primary
+    const [status, setStatus] = useState(prop.status);
+
+    // const genBorderColor = (status) => {
+    //     if (status === "Canceled") return "#FBE2C5"
+    //     else if (status === "Accepted") return "#D3EAE5"
+    //     else if (status === "Rejected") return "#FFD6C9"
+    //     else return COLORS.lightgray
+    // }
+    // const genStatusColor = (status) => {
+    //     if (status === "Canceled") return "#EF8C18"
+    //     else if (status === "Accepted") return COLORS.third
+    //     else if (status === "Rejected") return "#FF5D29"
+    //     else return COLORS.primary
+    // }
+    const genConfigStatus = () => {
+        switch(status){
+            case "Pending":
+                return {
+                    statusText:"Waiting for Approval",
+                    statusColor: COLORS.primary,
+                    borderColor: COLORS.lightgray,
+                };
+
+            case "Canceled":
+                return {
+                    statusText:"Canceled",
+                    statusColor:"#EF8C18",
+                    borderColor:"#FBE2C5",
+                };
+
+            case "Approved":
+                return {
+                    statusText:"Approved",
+                    statusColor:COLORS.third,
+                    borderColor:"#D3EAE5",
+                };
+
+            case "Rejected":
+                return {
+                    statusText:"Rejected",
+                    statusColor:"#FF5D29",
+                    borderColor:"#FFD6C9",
+                };
+            default:
+                return {
+                    statusText:"Unknown",
+                    statusColor:COLORS.primary,
+                    borderColor:COLORS.lightgray,
+                };
+        }
     }
 
+    const translateDateFormat = (timeStamp) => {
+        //2001-02-15T17:00:00.000+00:00
+        //            to be
+        //February 29, 2000 9:30 a.m."
+        var date = parseInt(timeStamp.substr(8, 2));
+        var month = parseInt(timeStamp.substr(5, 2));
+        var year = parseInt(timeStamp.substr(0, 4));;
+        const monthName = [
+            'January', 
+            'February', 
+            'March', 
+            'April', 
+            'May', 
+            'June', 
+            'July', 
+            'August', 
+            'September', 
+            'October', 
+            'November', 
+            'December']
+        return (monthName[month-1] + " " + date.toString() + ", " + year.toString());
+    }
+
+    const translateTimeFormat = (timeStamp) => {
+        var hour = timeStamp.substr(11, 2);
+        var min = timeStamp.substr(14, 2);
+        return hour.toString() + ":" + min.toString();
+    }
+
+    const translateSlotsListFormat = (subjectName, timeStamp, slot_no) => {
+        // return "1234"
+        // return slot_no.toString()
+        return subjectName + " [ " + (slot_no + 8).toString()  + ":00 - "+ (slot_no + 9).toString() + ":00 ] " + translateDateFormat(timeStamp)
+    }
     
     return (  
-        <div className="booking-card" style={{borderColor:genBorderColor(status)}}>
+        <div className="booking-card" style={{borderColor:genConfigStatus().borderColor}}>
             <div id="header">
-                <p style={{fontWeight:"500", color:genStatusColor(status)}}>
-                    {status}
+                <p style={{fontWeight:"500", color:genConfigStatus().statusColor}}>
+                    {genConfigStatus().statusText}
                 </p>
-                <p>request time: {requestTime}</p>
+                <p>request time: {translateDateFormat(requestTime)} {translateTimeFormat(requestTime)}</p>
             </div>
             
-            <div style={{width:"100%", height:"2px", backgroundColor:genBorderColor(status)}}></div>
+            <div style={{width:"100%", height:"2px", backgroundColor:genConfigStatus().borderColor}}></div>
             <p style={{marginBottom:"0px"}}>from</p>
             <div id="info-section">
                 <div id="text-zone">
-                    <h3>{tutorName}</h3>
+                    <h3>{studentName}</h3>
                     <p>total price: <b>{totalPrice} baht</b></p>
                 </div>
                 <div id="button-zone">
-                    {status === "Waiting for Approval"?
+                    {status === "Pending"?
                         <>
                         <button id="approve-button" onClick={() =>
                             {setShow(false)  
@@ -51,10 +131,19 @@ const BookingRequestCard= (prop) => {
                 </div>
             </div>
             <ul>
-                {subjectList.map((e) => (
+                {/* {subjectList.map((e) => (
                     <li>
                         {e}
                     </li>
+                ))} */}
+                {days.map((e,i) => (
+                    <div key={bookingId + i + bookingId}>
+                        {e.slots.map((slot_no,ii) => (
+                            <li key={bookingId + i + ii} style={{margin:"2px 0px"}}>
+                                {translateSlotsListFormat(e.subject[ii],e.date,slot_no)}
+                            </li>
+                        ))}
+                    </div>
                 ))}    
             </ul>
 
--- apps/tuto-real-frontend/src/app/components/navbar/bookingRequest/BookingRequestOverlay.js
+++ apps/tuto-real-frontend/src/app/components/navbar/bookingRequest/BookingRequestOverlay.js
@@ -1,8 +1,52 @@
 import { Overlay, Popover } from "react-bootstrap";
+import { useCallback, useEffect, useState } from "react";
+import { useAuth } from "../../../auth";
+import { client } from "../../../axiosConfig";
 import BookingRequestCard from "./BookingRequestCard";
 
 const BookingRequestOverlay = (prop) => {
     const {show,target,setShowModal,setShow} = prop;
+    const { _id } = useAuth();
+
+    const [bookingList, setBookingList] = useState([])
+
+    useEffect(() => {
+        //todo: uncomment statement belown if url is ready
+        //fetchData();
+        console.log(bookingList)
+    }, [bookingList]);
+
+    useEffect(() => {
+        // window.location.reload(false)
+    }, []);
+
+    const fetchData = useCallback(async () => {
+        console.log("fetch Booking of:",_id);
+        await client({
+            method: 'GET',
+            url: `/booking/tutor`,
+            params: {
+                // tutorId: `${}`,
+                _id: `${_id}`,
+                // _id: `${_id}`,
+            },
+        })
+        .then((data) => {
+            console.log(data.data.data);
+            // console.log(data.data.data[0].student_id.firstName);
+            setBookingList(data.data.data);
+        })
+        .catch((res) => {
+            console.log(res);
+        });
+    }, [_id, show])
+
+    useEffect(() => {
+        //todo: uncomment statement belown if url is ready
+        fetchData();
+        // console.log(bookingList)
+    }, [fetchData]);
+
     return (  
         <Overlay
             show={show}
@@ -15,11 +59,24 @@ const BookingRequestOverlay = (prop) => {
         >
             <Popover className="booking-overlay" id="popover-contained">
                 <Popover.Header as="h3">Booking Request</Popover.Header>
-                <Popover.Body> 
-                    <BookingRequestCard
+                <Popover.Body>
+                    {bookingList.map((e,i) => (
+                        <BookingRequestCard
+                            setShowModal= {setShowModal} 
+                            setShow= {setShow}
+                            key={e._id}
+                            bookingId={e._id}
+                            status={e.status}
+                            requestTime={e.timeStamp}
+                            studentName={e.student_id.firstName+" "+e.student_id.lastName}
+                            totalPrice={e.totalPrice}
+                            days={e.days}
+                        />
+                    ))} 
+                    {/* <BookingRequestCard
                         setShowModal= {setShowModal} 
                         setShow= {setShow}
-                        status="Waiting for Approval" 
+                        status="Pending" 
                         requestTime="February 29, 2000 9:30 a.m."
                         tutorName="Komsorn Sookdang"
                         totalPrice={200}
@@ -43,7 +100,7 @@ const BookingRequestOverlay = (prop) => {
                         ]}
                     />
                     <BookingRequestCard 
-                        status="Accepted" 
+                        status="Approved" 
                         requestTime="February 29, 2000 9:30 a.m."
                         tutorName="Komsorn Sookdang"
                         totalPrice={200}
@@ -65,7 +122,7 @@ const BookingRequestOverlay = (prop) => {
                             "Mathematic [ 14:00 - 15:00 ] February 30, 2000",
                             "Software Engineering [ 14:00 - 15:00 ] February 29, 2000"
                         ]}
-                    />
+                    /> */}
                 </Popover.Body>
             </Popover>
         </Overlay>
