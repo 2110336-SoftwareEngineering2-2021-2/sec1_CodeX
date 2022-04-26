da667ba840b57fb2febe526904214bbd3d64573b
Beam <beam7684@gmail.com>
2022-04-26 13:41:39 +0700

    [Deploy] Direct access url bug fix + add key folder

--- start_production.sh
+++ start_production.sh
@@ -2,8 +2,7 @@
 
 # NODE_ENV=production node dist/apps/tuto-real-backend/main.js &
 cd dist/apps/tuto-real-frontend/
-# ../../../node_modules/.bin/webpack start -p 4200 &
-../../../node_modules/.bin/serve -l 4200  &
-# npm install -D webpack-cli &
-echo "Started TutoReal Frontend"
+# ../../../node_modules/.bin/serve -l 4200  &
+../../../node_modules/.bin/pm2 serve ./ 4200 --spa  &
+echo "Started TutoReal Frontend using pm2"
 cd ../../../
