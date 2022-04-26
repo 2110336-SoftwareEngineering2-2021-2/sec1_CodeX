3129da400333975573aca9f68cecb60171192c66
Beam <beam7684@gmail.com>
2022-04-26 14:11:48 +0700

    Docker railway deployment test

--- start_production.sh
+++ start_production.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# NODE_ENV=production node dist/apps/tuto-real-backend/main.js &
+NODE_ENV=production node dist/apps/tuto-real-backend/main.js &
 cd dist/apps/tuto-real-frontend/
 # ../../../node_modules/.bin/serve -l 4200  &
 ../../../node_modules/.bin/pm2 serve ./ 4200 --spa  &
